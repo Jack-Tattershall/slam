6dbee531128096a8332c8790709cec928be3a847
jatattershall <jack.t.tattershall@dyson.com>
2023-03-03 20:43:06 +0000

    extract and draw keypoints

--- slam.py
+++ slam.py
@@ -1,11 +1,15 @@
 import cv2
 import numpy as np
+import time
+from extractor import KeypointExtractor
 
 fname = "data/sample.mp4"
 W = 1024
 H = 512
 
 cap = cv2.VideoCapture(fname)
+extractor = KeypointExtractor()
+
 
 try:
     while cap.isOpened():
@@ -15,12 +19,16 @@ try:
         if ret:
 
             frame = cv2.resize(frame, (W, H), interpolation=cv2.INTER_AREA)
-            print(frame.shape)
+            kp, des = extractor.extract_keypoints(frame)
+            frame = cv2.drawKeypoints(frame, kp, None, color=(0, 255, 0), flags=0)
+            print(des.shape)
 
-            cv2.imshow("frame", frame)
+            cv2.imshow(fname, frame)
             cv2.waitKey(1)
 
+
 except KeyboardInterrupt:
+
     cap.release()
     cv2.destroyAllWindows()
     cv2.waitKey(0)
