95434bae83774cba62c4570fd037780fdf307132
jatattershall <jack.t.tattershall@dyson.com>
2023-03-03 23:41:50 +0000

    match kps

--- extractor.py
+++ extractor.py
@@ -4,9 +4,9 @@ import cv2
 
 class KeypointExtractor:
     def __init__(self) -> None:
-        # self.H = H
-        # self.W = W
+
         self.orb = cv2.ORB_create()
+        self.bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 
     def extract_keypoints(self, img):
 
@@ -14,3 +14,18 @@ class KeypointExtractor:
         kp, des = self.orb.compute(img, kp)
 
         return kp, des
+
+    def match_keypoints(self, des1, des2):
+
+        matches = self.bf.match(des1, des2)
+        matches = sorted(matches, key=lambda x: x.distance)
+
+        return matches
+
+    def compute_lines(self, matches):
+
+        dist = [match.distance for match in matches]
+        frame_idxs = [match.queryIdx for match in matches]
+        last_frame_idxs = [match.trainIdx for match in matches]
+
+        print(dist[0], frame_idxs[0], last_frame_idxs[0])
--- slam.py
+++ slam.py
@@ -10,21 +10,29 @@ H = 512
 cap = cv2.VideoCapture(fname)
 extractor = KeypointExtractor()
 
-
+is_first = True
 try:
     while cap.isOpened():
 
         ret, frame = cap.read()
+        if is_first:
+            last_frame = frame
+            is_first = False
 
         if ret:
 
             frame = cv2.resize(frame, (W, H), interpolation=cv2.INTER_AREA)
-            kp, des = extractor.extract_keypoints(frame)
-            frame = cv2.drawKeypoints(frame, kp, None, color=(0, 255, 0), flags=0)
-            print(des.shape)
+            kp_frame, des_frame = extractor.extract_keypoints(frame)
+            kp_last_frame, des_last_frame = extractor.extract_keypoints(last_frame)
+            matches = extractor.match_keypoints(des_frame, des_last_frame)
+            extractor.compute_lines(matches)
+
+            frame = cv2.drawKeypoints(frame, kp_frame, None, color=(0, 255, 0), flags=0)
 
             cv2.imshow(fname, frame)
             cv2.waitKey(1)
+
+            last_frame = frame
 
 
 except KeyboardInterrupt:
